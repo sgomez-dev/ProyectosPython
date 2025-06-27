1045f50566259a9a84e56df6072f061ec6f703fc
Santiago Gómez de la Torre Romero <81492002+sgomez-dev@users.noreply.github.com>
2025-06-27 13:49:33 +0200

    Update AutomatizaciónSelenium.py

--- Proyectos/BOTS/AutomatizaciónSelenium.py
+++ Proyectos/BOTS/AutomatizaciónSelenium.py
@@ -26,9 +26,9 @@ conf_pass_field = driver.find_element(By.ID, "confPass")
 
 name_field.send_keys("Santiago")
 last_name_field.send_keys("Gómez")
-email_field.send_keys("contacto@sgomez.dev")
-pass_field.send_keys("Santigt1503.")
-conf_pass_field.send_keys("Santigt1503.")
+email_field.send_keys(".....@sgomez.dev")
+pass_field.send_keys("........")
+conf_pass_field.send_keys("........")
 
 register_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Registrar')]")
 register_button.click()
